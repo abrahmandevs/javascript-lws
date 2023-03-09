d5d5e032681ab65cf2452bd83f9fc5d048319761
ARRahman73 <dra3190@gmail.com>
2023-03-09 22:22:31 +0600

    sets

--- class41-Sets/script.js
+++ class41-Sets/script.js
@@ -1,7 +1,28 @@
-// sets
-// create a sets new Set()
-// add a sets add()
+/* 
+A JavaScript Set is a collection of unique values.
+Each value can only occur once in a Set.
+
+sets
+create a sets new Set()
+add a sets add() 
+size() [method]
+*/
+let ary = ['a', 'b', 'c'];
 const myset = new Set([1, 2, 3, 4, 5, 6, 7, 8]);
+// console.log(myset.size)
+(function (value) { /// add sets
+	for (let x = 0; (x < value.size || x < value.length); x++) {
+		let newsets = [x].sort(function (a, b) {
+			return a - b;
+		});
+		let sets = newsets.toString();
+		for (let x of sets) {
+			myset.add(x);
+			console.log(x)
+		}
+		// console.log([x].sort())
+	}
+})(myset)
 
 let getSet = '', getSet2 = '';
 myset.forEach(function (value) {
@@ -13,3 +34,4 @@ for (let x of myset.values()) {
 }
 document.getElementById('div1').innerHTML = getSet;
 document.getElementById('div2').innerHTML = getSet2;
+
