21617acb752f16c3e9a1bb0e96c35b728ec8d7db
ARRahman73 <dra3190@gmail.com>
2023-03-29 03:13:37 +0600

    js AJAX example and lws end

--- class117-121-ajax/script.js
+++ class117-121-ajax/script.js
@@ -24,3 +24,54 @@ loadData.addEventListener('click', () => {
     // sent data
     xhr.send()
 });
+// response and request
+function sendRequest(method, url, data) {
+    const promise = new Promise((resolve, reject) => {
+        // create a new request;
+        const xhr = new XMLHttpRequest();
+
+        // what to do when request arrive
+        xhr.onload = function () {
+            // console.log(JSON.parse(this.responseText).userId);
+            // console.log(this.response);
+            // handle application error
+            if (this.status >= 400) {
+                reject(`there was an application error and response status is ${this.status} and response text is ${this.statusText}`);
+            } else {
+                resolve(this.response)
+            }
+        }
+        xhr.onerror = function () {
+            reject("there was an error");
+        }
+
+        xhr.open(method, url);
+        xhr.responseType = "json";
+        xhr.send(data);
+    });
+    return promise;
+}
+function get() {
+    sendRequest("GET", "https://jsonplaceholder.typicode.com/todos/1").then((responseData) => {
+        console.log(responseData);
+    }).catch(err => {
+        console.log(err);
+    });
+}
+function sent() {
+    sendRequest("POST", "https://jsonplaceholder.typicode.com/posts", JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+    })).then((responseData) => {
+        console.log(responseData);
+    });
+}
+
+
+let getData = document.getElementById('getData');
+let sentData = document.getElementById('sentData');
+
+
+getData.addEventListener('click', get);
+sentData.addEventListener('click', sent);
